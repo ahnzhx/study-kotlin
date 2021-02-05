ad6a7d8295edceae0dc6c45f86818716c5bb03f5
ahnzhx <modunaeggu@naver.com>
2021-02-05 20:34:58 +0900

    kotlin in action ch08

--- src/main/kotlin/kotlinInAction/ch06/NullPossibility.kt
+++ src/main/kotlin/kotlinInAction/ch06/NullPossibility.kt
@@ -4,7 +4,14 @@ package kotlinInAction.ch06
 fun<T> printHashCode1(t: T) = println(t?.hashCode()) // t는 null이 가능
 fun<T: Any> printHashCode2(t: T) = println(t.hashCode()) // t는 null이 불가능
 
-fun yellAt(person: PersonSample) = println(person.name.toUpperCase() + "!!!")
+//fun yellAt(person: PersonSample) = println(person.name.toUpperCase() + "!!!")
 fun main(){
-    yellAt(PersonSample(null))
+//    yellAt(PersonSample(null))
+    println(collectionWithNull())
 }
+
+fun collectionWithNull(): List<Int> {
+    val listWithNull = listOf(1, null, 3)
+    return listWithNull.filterNotNull()
+}
+
